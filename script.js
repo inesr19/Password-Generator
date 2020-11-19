79215287b87f211dc2cfa273ce3c8d93d5e0db2d
Ines Radic <inesradic12314@gmail.com>
2020-11-18 21:59:43 -0800

    add character variables

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Numeric characters
+var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+
+// Alphabetical characters
+var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "z",];
+
+// Special characters
+var char = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +23,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+var generateBtn = prompt("How many characters would you like your password to have? Chose from 8 to 128 characters.")
