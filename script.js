8c5fef1bbbd942e2e123d383c8375a468c673e6e
Ines Radic <inesradic12314@gmail.com>
2020-11-19 13:33:42 -0800

    update vars and add inital prompt

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Character object
+var enterNumberChar;
+var lowercase;
+var uppercase;
+var number;
+var specialChar;
+
 // Numeric characters
-var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
 
 // Alphabetical characters
-var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "z",];
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "z",];
 
 // Special characters
-var char = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+char = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+for (var i = 0; i < enterNumberChar; i++) {
+  var characters = Math.floor(Math.random() * 128) + 1;
+}
+
 
 
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(characters) {
+  enterNumberChar = parseInt(prompt("How many character would you like your password to have? Chose between 8 to 128"));
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.getElementById("#password").placeholder = password;
 
   passwordText.value = password;
 
@@ -23,4 +36,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-var generateBtn = prompt("How many characters would you like your password to have? Chose from 8 to 128 characters.")
