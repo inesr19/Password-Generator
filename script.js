6898ecd4936aaaf3b684b3c1a31eed4f1a7ba2a1
Ines Radic <inesradic12314@gmail.com>
2020-11-20 16:33:37 -0800

    add if and else statements

--- script.js
+++ script.js
@@ -1,53 +1,71 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Character prompts
-var enterNumberChar;
-var lowercase;
-var uppercase;
-var number;
-var specialChar;
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Numeric characters
-num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
-
-// Alphabetical characters
-alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "z",];
-
+const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+// Lowercase alphabet
+const alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "z",];
+// Uppercase alphabet
+const alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "Y", "Z",];
 // Special characters
-char = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+const char = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
-for (var i = 0; i < enterNumberChar; i++) {
-  var characters = Math.floor(Math.random() * 128) + 1;
-  console.log()
-}
-
-
+var options = [];
+var generatePassword = "";
 
 
 // Write password to the #password input
 function writePassword() {
-  enterNumberChar = parseInt(prompt("How many characters would you like your password to have? Chose between 8 to 128"));
 
-  lowercase = confirm("Would you like lowercase letters?")
-
-  uppercase = confirm("Would you like uppercase letters?")
-
-  number = confirm("Would you like numbers?")
-
-  specialChar = confirm("Would you like special character?")
-  
-
-  
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+// Initial prompt
+length = parseInt(prompt("How many characters would you like your password to have? Chose between 8 to 128"));
+if (length < 8) {
+  alert("Your password must be more than 8 characters.")
+} else if (length > 128) {
+  alert("Your password must be less than 128 characters.")
 }
 
+var random = Math.floor(Math.random() * options.length)
+
+// If the user wants lower case letters.
+alphaLower = confirm("Would you like lowercase letters?")
+if (lowercase === true) {
+  console.log("includes letters");
+}
+  
+alphaUpper = confirm("Would you like uppercase letters?")
+if (uppercase === true) {
+  console.log("includes uppercase letters");
+}
+
+num = confirm("Would you like numbers?")
+if (num === true) {
+  console.log("includes numbers");
+}
+
+char = confirm("Would you like special character?")
+if (char === true) {
+  console.log("includes special characters.");
+}
+    
+  var password = generatePassword();
+  console.log("Password length: " + password.length);
+
+  
+  for (var i = 0; i < options.length; i++) {
+    password += options[random]
+    console.log(password);
+  }
+  console.log(password);
+
+  var passwordText = document.querySelector("#password");
+  passwordText.textContent.value = password;
+    
+  } 
 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
